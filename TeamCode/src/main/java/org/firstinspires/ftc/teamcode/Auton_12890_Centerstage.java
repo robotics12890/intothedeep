a8d4dd91659341685d5cd016266a7d56fda8b0d0
EmersonRobotics12890 <robotics12890@emerson-school.org>
2023-11-03 17:05:09 -0400

    Added constructor method for Auton and started Auton F4.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auton_12890_Centerstage.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auton_12890_Centerstage.java
@@ -31,8 +31,7 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.util.ElapsedTime;
+
 
 /*
  * This OpMode illustrates the concept of driving a path based on encoder counts.
@@ -65,178 +64,29 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 public class Auton_12890_Centerstage extends LinearOpMode {
 
     /* Declare OpMode members. */
-    private DcMotor leftFrontDrive = null;
-    private DcMotor leftBackDrive = null;
-    private DcMotor rightFrontDrive = null;
-    private DcMotor rightBackDrive = null;
-
-    private ElapsedTime runtime = new ElapsedTime();
-
-    // Calculate the COUNTS_PER_INCH for your specific drive train.
-    // Go to your motor vendor website to determine your motor's COUNTS_PER_MOTOR_REV
-    // For external drive gearing, set DRIVE_GEAR_REDUCTION as needed.
-    // For example, use a value of 2.0 for a 12-tooth spur gear driving a 24-tooth spur gear.
-    // This is gearing DOWN for less speed and more torque.
-    // For gearing UP, use a gear ratio less than 1.0. Note this will affect the direction of wheel rotation.
-    static final double COUNTS_PER_MOTOR_REV = 537.7;    // eg: TETRIX Motor Encoder
-    static final double DRIVE_GEAR_REDUCTION = 1.0;     // No External Gearing.
-    static final double WHEEL_DIAMETER_CENTIMETERS = 9.6;     // For figuring circumference
-    static final double COUNTS_PER_CENTIMETERS = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) /
-            (WHEEL_DIAMETER_CENTIMETERS * 3.1415);
-//    static final double     DRIVE_SPEED             = 0.6;
-//    static final double     TURN_SPEED              = 0.5;
+    Robot robot = new Robot(hardwareMap);
 
     @Override
     public void runOpMode() {
-
-        // Initialize the drive system variables.
-        leftFrontDrive = hardwareMap.get(DcMotor.class, "left_front_drive");
-        leftBackDrive = hardwareMap.get(DcMotor.class, "left_back_drive");
-        rightFrontDrive = hardwareMap.get(DcMotor.class, "right_front_drive");
-        rightBackDrive = hardwareMap.get(DcMotor.class, "right_back_drive");
-
-        leftFrontDrive.setDirection(DcMotor.Direction.FORWARD);
-        leftBackDrive.setDirection(DcMotor.Direction.FORWARD);
-        rightFrontDrive.setDirection(DcMotor.Direction.REVERSE);
-        rightBackDrive.setDirection(DcMotor.Direction.REVERSE);
-
-        leftFrontDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        rightFrontDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        leftBackDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        rightBackDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-
-//        leftFrontDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-//        rightFrontDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-//        leftBackDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-//        rightBackDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-
         // Send telemetry message to indicate successful Encoder reset
         telemetry.addData("Starting at", "%7d :%7d :%7d :%7d",
-                leftFrontDrive.getCurrentPosition(),
-                rightFrontDrive.getCurrentPosition(),
-                leftBackDrive.getCurrentPosition(),
-                rightBackDrive.getCurrentPosition());
+                robot.leftFrontDrive.getCurrentPosition(),
+                robot.rightFrontDrive.getCurrentPosition(),
+                robot.leftBackDrive.getCurrentPosition(),
+                robot.rightBackDrive.getCurrentPosition());
         telemetry.update();
 
         // Wait for the game to start (driver presses PLAY)
         waitForStart();
 
-        // Step through each leg of the path,
-        // Note: Reverse movement is obtained by setting a negative distance (not speed)
-//        encoderDrive(1, -24, 24, 24,-24);  // S1: Forward 47 Inches with 5 Sec timeout
-//        encoderDrive(1, 12, -12, 4.0, 12 );  // S2: Turn Right 12 Inches with 4 Sec timeout
-//        encoderDrive(1, -24, -24, 4.0, );  // S3: Reverse 24 Inches with 4 Sec timeout
-
-
         //algorithm
-        driveBackward(30.5, 0.5);
-        strafeLeft(30.5, 0.5);
-        driveForward(30.5, 0.5);
-        strafeRight(30.5, 0.5);
-//        driveBackward(30.5, 0.5);
-//        strafeLeft(30.5, 0.5);
-//        driveForward(30.5, 0.5);
-//        strafeRight(30.5, 0.5);
-//        driveBackward(30.5, 0.5);
-//        strafeLeft(30.5, 0.5);
-//        driveForward(30.5, 0.5);
-//        strafeRight(30.5, 0.5);
+        robot.driveForward(30.5, 1);
+        robot.strafeLeft(30.5, 1);
+        robot.driveBackward(30.5, 1);
+        robot.strafeRight(30.5, 1);
 
         telemetry.addData("Path", "Complete");
         telemetry.update();
         sleep(1000);  // pause to display final telemetry message.
-    }
-
-    // move forward / backward function
-    public void drive(double distanceCm, double power) {
-        int leftFrontTargetPosition = leftFrontDrive.getCurrentPosition() - (int) (distanceCm * COUNTS_PER_CENTIMETERS);
-        int leftBackTargetPosition = leftBackDrive.getCurrentPosition() - (int) (distanceCm * COUNTS_PER_CENTIMETERS);
-        int rightFrontTargetPosition = rightFrontDrive.getCurrentPosition() - (int) (distanceCm * COUNTS_PER_CENTIMETERS);
-        int rightBackTargetPosition = rightBackDrive.getCurrentPosition() - (int) (distanceCm * COUNTS_PER_CENTIMETERS);
-
-        leftFrontDrive.setTargetPosition(leftFrontTargetPosition);
-        leftBackDrive.setTargetPosition(leftBackTargetPosition);
-        rightFrontDrive.setTargetPosition(rightFrontTargetPosition);
-        rightBackDrive.setTargetPosition(rightBackTargetPosition);
-
-        leftFrontDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        leftBackDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        rightFrontDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        rightBackDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-
-        leftFrontDrive.setPower(Math.abs(power));
-        leftBackDrive.setPower(Math.abs(power));
-        rightFrontDrive.setPower(Math.abs(power));
-        rightBackDrive.setPower(Math.abs(power));
-
-        while (leftFrontDrive.isBusy() && leftBackDrive.isBusy() && rightFrontDrive.isBusy() && rightBackDrive.isBusy()) {
-        }
-
-        leftFrontDrive.setPower(0);
-        leftBackDrive.setPower(0);
-        rightFrontDrive.setPower(0);
-        rightBackDrive.setPower(0);
-
-//         Display it for the driver.
-        telemetry.addData("Running to", " %7d :%7d :%7d :%7d", leftFrontTargetPosition, leftBackTargetPosition, rightFrontTargetPosition, rightBackTargetPosition);
-        telemetry.addData("Currently at", " at %7d :%7d :%7d :%7d",
-                leftFrontDrive.getCurrentPosition(), rightFrontDrive.getCurrentPosition(), leftBackDrive.getCurrentPosition(), rightBackDrive.getCurrentPosition());
-        telemetry.update();
-    }
-
-    public void driveBackward(double distanceCm, double power) {
-        double backwardsDistance = -(Math.abs(distanceCm));
-        drive(backwardsDistance, power);
-    }
-
-    public void driveForward(double distanceCm, double power) {
-        double forwardsDistance = (Math.abs(distanceCm));
-        drive(forwardsDistance, power);
-    }
-
-    public void strafe(double distanceCm, double power) {
-        int leftFrontTargetPosition = leftFrontDrive.getCurrentPosition() + (int) (distanceCm * COUNTS_PER_CENTIMETERS);
-        int leftBackTargetPosition = leftBackDrive.getCurrentPosition() - (int) (distanceCm * COUNTS_PER_CENTIMETERS);
-        int rightFrontTargetPosition = rightFrontDrive.getCurrentPosition() - (int) (distanceCm * COUNTS_PER_CENTIMETERS);
-        int rightBackTargetPosition = rightBackDrive.getCurrentPosition() + (int) (distanceCm * COUNTS_PER_CENTIMETERS);
-
-        leftFrontDrive.setTargetPosition(leftFrontTargetPosition);
-        leftBackDrive.setTargetPosition(leftBackTargetPosition);
-        rightFrontDrive.setTargetPosition(rightFrontTargetPosition);
-        rightBackDrive.setTargetPosition(rightBackTargetPosition);
-
-        leftFrontDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        leftBackDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        rightFrontDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        rightBackDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-
-        leftFrontDrive.setPower(Math.abs(power));
-        leftBackDrive.setPower(Math.abs(power));
-        rightFrontDrive.setPower(Math.abs(power));
-        rightBackDrive.setPower(Math.abs(power));
-
-        while (leftFrontDrive.isBusy() && leftBackDrive.isBusy() && rightFrontDrive.isBusy() && rightBackDrive.isBusy()) {
-        }
-
-        leftFrontDrive.setPower(0);
-        leftBackDrive.setPower(0);
-        rightFrontDrive.setPower(0);
-        rightBackDrive.setPower(0);
-
-//         Display it for the driver.
-        telemetry.addData("Running to", " %7d :%7d :%7d :%7d", leftFrontTargetPosition, leftBackTargetPosition, rightFrontTargetPosition, rightBackTargetPosition);
-        telemetry.addData("Currently at", " at %7d :%7d :%7d :%7d",
-                leftFrontDrive.getCurrentPosition(), rightFrontDrive.getCurrentPosition(), leftBackDrive.getCurrentPosition(), rightBackDrive.getCurrentPosition());
-        telemetry.update();
-    }
-
-    public void strafeRight(double distanceCm, double power) {
-        double strafeRightDistance = -(Math.abs(distanceCm));
-        strafe(strafeRightDistance, power);
-    }
-
-    public void strafeLeft(double distanceCm, double power) {
-        double strafeLeftDistance = (Math.abs(distanceCm));
-        strafe(strafeLeftDistance, power);
     }
 }
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
@@ -0,0 +1,148 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+public class Robot {
+
+    /* Declare OpMode members. */
+    public DcMotor leftFrontDrive = null;
+    public DcMotor leftBackDrive = null;
+    public DcMotor rightFrontDrive = null;
+    public DcMotor rightBackDrive = null;
+
+    public ElapsedTime runtime = new ElapsedTime();
+
+    // Calculate the COUNTS_PER_INCH for your specific drive train.
+    // Go to your motor vendor website to determine your motor's COUNTS_PER_MOTOR_REV
+    // For external drive gearing, set DRIVE_GEAR_REDUCTION as needed.
+    // For example, use a value of 2.0 for a 12-tooth spur gear driving a 24-tooth spur gear.
+    // This is gearing DOWN for less speed and more torque.
+    // For gearing UP, use a gear ratio less than 1.0. Note this will affect the direction of wheel rotation.
+    static final double COUNTS_PER_MOTOR_REV = 537.7;    // eg: TETRIX Motor Encoder
+    static final double DRIVE_GEAR_REDUCTION = 1.0;     // No External Gearing.
+    static final double WHEEL_DIAMETER_CENTIMETERS = 9.6;     // For figuring circumference
+    static final double COUNTS_PER_CENTIMETERS = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) /
+            (WHEEL_DIAMETER_CENTIMETERS * 3.1415);
+
+
+    public Robot(HardwareMap hardwareMap) {
+
+        // Initialize the drive system variables.
+        leftFrontDrive = hardwareMap.get(DcMotor.class, "left_front_drive");
+        leftBackDrive = hardwareMap.get(DcMotor.class, "left_back_drive");
+        rightFrontDrive = hardwareMap.get(DcMotor.class, "right_front_drive");
+        rightBackDrive = hardwareMap.get(DcMotor.class, "right_back_drive");
+
+        leftFrontDrive.setDirection(DcMotor.Direction.FORWARD);
+        leftBackDrive.setDirection(DcMotor.Direction.FORWARD);
+        rightFrontDrive.setDirection(DcMotor.Direction.REVERSE);
+        rightBackDrive.setDirection(DcMotor.Direction.REVERSE);
+
+        leftFrontDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rightFrontDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        leftBackDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rightBackDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        leftFrontDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rightFrontDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        leftBackDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rightBackDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+
+
+    }
+    // move forward / backward function
+    public void drive(double distanceCm, double power) {
+        int leftFrontTargetPosition = leftFrontDrive.getCurrentPosition() - (int) (distanceCm * COUNTS_PER_CENTIMETERS);
+        int leftBackTargetPosition = leftBackDrive.getCurrentPosition() - (int) (distanceCm * COUNTS_PER_CENTIMETERS);
+        int rightFrontTargetPosition = rightFrontDrive.getCurrentPosition() - (int) (distanceCm * COUNTS_PER_CENTIMETERS);
+        int rightBackTargetPosition = rightBackDrive.getCurrentPosition() - (int) (distanceCm * COUNTS_PER_CENTIMETERS);
+
+        leftFrontDrive.setTargetPosition(leftFrontTargetPosition);
+        leftBackDrive.setTargetPosition(leftBackTargetPosition);
+        rightFrontDrive.setTargetPosition(rightFrontTargetPosition);
+        rightBackDrive.setTargetPosition(rightBackTargetPosition);
+
+        leftFrontDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        leftBackDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        rightFrontDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        rightBackDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        leftFrontDrive.setPower(Math.abs(power));
+        leftBackDrive.setPower(Math.abs(power));
+        rightFrontDrive.setPower(Math.abs(power));
+        rightBackDrive.setPower(Math.abs(power));
+
+        while (leftFrontDrive.isBusy() && leftBackDrive.isBusy() && rightFrontDrive.isBusy() && rightBackDrive.isBusy()) {
+        }
+
+        leftFrontDrive.setPower(0);
+        leftBackDrive.setPower(0);
+        rightFrontDrive.setPower(0);
+        rightBackDrive.setPower(0);
+
+//         Display it for the driver.
+//        telemetry.addData("Running to", " %7d :%7d :%7d :%7d", leftFrontTargetPosition, leftBackTargetPosition, rightFrontTargetPosition, rightBackTargetPosition);
+//        telemetry.addData("Currently at", " at %7d :%7d :%7d :%7d",
+//                leftFrontDrive.getCurrentPosition(), rightFrontDrive.getCurrentPosition(), leftBackDrive.getCurrentPosition(), rightBackDrive.getCurrentPosition());
+//        telemetry.update();
+    }
+
+    public void driveBackward(double distanceCm, double power) {
+        double backwardsDistance = -(Math.abs(distanceCm));
+        drive(backwardsDistance, power);
+    }
+
+    public void driveForward(double distanceCm, double power) {
+        double forwardsDistance = (Math.abs(distanceCm));
+        drive(forwardsDistance, power);
+    }
+
+    public void strafe(double distanceCm, double power) {
+        int leftFrontTargetPosition = leftFrontDrive.getCurrentPosition() + (int) (distanceCm * COUNTS_PER_CENTIMETERS);
+        int leftBackTargetPosition = leftBackDrive.getCurrentPosition() - (int) (distanceCm * COUNTS_PER_CENTIMETERS);
+        int rightFrontTargetPosition = rightFrontDrive.getCurrentPosition() - (int) (distanceCm * COUNTS_PER_CENTIMETERS);
+        int rightBackTargetPosition = rightBackDrive.getCurrentPosition() + (int) (distanceCm * COUNTS_PER_CENTIMETERS);
+
+        leftFrontDrive.setTargetPosition(leftFrontTargetPosition);
+        leftBackDrive.setTargetPosition(leftBackTargetPosition);
+        rightFrontDrive.setTargetPosition(rightFrontTargetPosition);
+        rightBackDrive.setTargetPosition(rightBackTargetPosition);
+
+        leftFrontDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        leftBackDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        rightFrontDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        rightBackDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        leftFrontDrive.setPower(Math.abs(power));
+        leftBackDrive.setPower(Math.abs(power));
+        rightFrontDrive.setPower(Math.abs(power));
+        rightBackDrive.setPower(Math.abs(power)); while (leftFrontDrive.isBusy() && leftBackDrive.isBusy() && rightFrontDrive.isBusy() && rightBackDrive.isBusy()) {
+        }
+
+
+
+        leftFrontDrive.setPower(0);
+        leftBackDrive.setPower(0);
+        rightFrontDrive.setPower(0);
+        rightBackDrive.setPower(0);
+
+//         Display it for the driver.
+//        telemetry.addData("Running to", " %7d :%7d :%7d :%7d", leftFrontTargetPosition, leftBackTargetPosition, rightFrontTargetPosition, rightBackTargetPosition);
+//        telemetry.addData("Currently at", " at %7d :%7d :%7d :%7d",
+//                leftFrontDrive.getCurrentPosition(), rightFrontDrive.getCurrentPosition(), leftBackDrive.getCurrentPosition(), rightBackDrive.getCurrentPosition());
+//        telemetry.update();
+    }
+
+    public void strafeRight(double distanceCm, double power) {
+        double strafeRightDistance = -(Math.abs(distanceCm));
+        strafe(strafeRightDistance, power);
+    }
+
+    public void strafeLeft(double distanceCm, double power) {
+        double strafeLeftDistance = (Math.abs(distanceCm));
+        strafe(strafeLeftDistance, power);
+    }
+}
