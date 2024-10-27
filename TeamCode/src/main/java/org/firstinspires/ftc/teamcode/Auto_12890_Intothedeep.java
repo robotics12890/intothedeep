c0f361ed58d12fea806cfc3f60fb4616349e951d
EmersonRobotics12890 <robotics12890@emerson-school.org>
2024-10-27 16:12:56 -0400

    Commented out in-progress auto program so we could re push code after update.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto_12890_Intothedeep.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto_12890_Intothedeep.java
@@ -1,8 +1,8 @@
-package org.firstinspires.ftc.teamcode;
-import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-@Autonomous(name = "Robot: Auto Intothedeep 12890 starting at A5 push to net zone", group = "Robot")
-public class Auto_12890_Intothedeep extends LinearOpMode {
-}
-//1 tile is 60 CM
-Robot robot = new Robot();
+//package org.firstinspires.ftc.teamcode;
+//import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+//import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+//@Autonomous(name = "Robot: Auto Intothedeep 12890 starting at A5 push to net zone", group = "Robot")
+//public class Auto_12890_Intothedeep extends LinearOpMode {
+//}
+////1 tile is 60 CM
+//Robot robot = new Robot();
